eb3e084de5116eda7ed3488da11a9c5721b0e339
U-Zyn Chua <chua@uzyn.com>
2016-04-21 16:44:47 +0800

    airbnb js eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ module.exports = {
   entry: path.resolve(__dirname, 'app/main.jsx'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['babel']
-      }
-    ]
-  }
+        loaders: ['babel'],
+      },
+    ],
+  },
 };
