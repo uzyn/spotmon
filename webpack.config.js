3255069135f7ea36010c8ad22ce6b5203d67695b
U-Zyn Chua <chua@uzyn.com>
2016-04-21 16:32:58 +0800

    init based on jkup/webpack-react-starter

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'app/main.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel']
+      }
+    ]
+  }
+};
