71fb585d59df61973479ae337c4e50d310083201
U-Zyn Chua <chua@uzyn.com>
2016-04-23 14:08:50 +0800

    Buildable

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/main.jsx'),
+  entry: path.resolve(__dirname, 'app/js/main'),
+  devServer: {
+    outputPath: path.join(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build/js'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        context: path.resolve(__dirname, 'app/static'),
+        from: '**/*',
+        to: path.resolve(__dirname, 'build'),
+      },
+    ]),
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
         loaders: ['babel'],
       },
     ],
